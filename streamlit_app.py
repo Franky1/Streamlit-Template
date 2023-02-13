544e914c9448cdd6fd30c359802018f0a85abdca
Franky1 <arvid.teichtmann@gmx.de>
2023-02-13 22:51:33 +0100

    added custom css

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,17 @@
 import streamlit as st
 
 
-if __name__ == "__main__":
-    st.set_page_config(page_title="Streamlit Template",
+# set basic page config
+st.set_page_config(page_title="Streamlit Template",
                     page_icon='✅',
                     initial_sidebar_state='collapsed')
+
+# apply custom css if needed
+# with open('utils/style.css') as css:
+#     st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
+
+
+if __name__ == "__main__":
     st.title('🔨 Streamlit Template')
     st.markdown("""
         This app is only a template for a new Streamlit project. <br>
